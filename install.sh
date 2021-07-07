bf2016b21115e03d149a099340b1ad496f4203ab
Jose Angelo Abarentos <jiggsfoo@gmail.com>
2021-07-07 23:56:46 +0800

    update setup

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 echo "Setting up your Mac..."
 
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew recipes
@@ -14,11 +19,11 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'not4anyone'; FLUSH PRIVILEGES;"
+# Set default MySQL root password and auth type
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install memcached imagick
+pecl install imagick memcached redis swoole
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose
@@ -26,12 +31,13 @@ pecl install memcached imagick
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
 
-# Install LTS version of node using nvm
-nvm install --lts
-
 # Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Sites
+
+# Create sites subdirectories
+mkdir $HOME/Sites/blade-ui-kit
+mkdir $HOME/Sites/eventsauce
+mkdir $HOME/Sites/laravel
 
 # Clone Github repositories
 ./clone.sh
@@ -43,6 +49,5 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
+# Set macOS preferences - we will run this last because this will reload the shell
 source .macos
